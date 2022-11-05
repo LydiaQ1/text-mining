91edef2b282126fd4f31c87749565216e86f8d0c
LydiaQ1 <112520633+LydiaQ1@users.noreply.github.com>
2022-11-04 23:06:57 -0400

    assignment 2.py

--- Assignments/assignment_2.py
+++ Assignments/assignment_2.py
@@ -5,6 +5,7 @@ nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import sys
 from unicodedata import category
+import string
 
 """
 Functions below are to have a general overview at the important features of the news article, on aspects include: title,
@@ -29,14 +30,19 @@ print(f"Keywords: {keywords}")
 print(f"Summary: {summary}")
 #print(article.text)
 
-def most_common(s):
+def most_common(s, excluding_stopwords=True):
     """
-    This function will return dictionary with words in the text and how many time each of them have appeared, the words will be 
+    This function will return dictionary with words in the text and how many times each of them have appeared, the words will be 
     sorted from a descending frequency.
     """
     d = dict()
-    text_d = s.split
-    for word in text_d():
+    text_d = s.split() # text_d is a list of words
+    if excluding_stopwords:
+        filename = "/Users/z/Desktop/2022 Fall/OIM/text-mining/Assignments/stopwords.txt"
+        stopword_set = stop_words(filename)
+    for word in text_d:
+        if word in stopword_set:
+            continue
         if word in d:
             d[word] += 1
         else:
@@ -47,34 +53,49 @@ def most_common(s):
 
 def stop_words(filename):
     """
-    This function will return dictionary with words in an appointed file (which will be addressed in the code below).
+    This function will return a set with words in an appointed file (which will be addressed in the code below).
     """
     f = open(filename, encoding = 'UTF8')
-    stop_word_d = {}
+    stop_word_set = set()
     for line in f:
         word = line.strip()
-        strop_word_d[word] = 0
-    return stop_word_d
+        stop_word_set.add(word)
+    return stop_word_set
+
+
+def clean_text(text):
+    """
+    This function will return text that have punctuations removed and converts all uppercase letters with lowercase letters.
+    """
+    for char in text:
+        if char in string.punctuation:
+            text = text.replace(char, '')
+            text = text.strip()
+            text = text.lower()
+    return text
 
 def main():
     """
     The functions below will return final output, which is the words in text with the values of how 
     many times they've appeared in a descending order, without stopwords in the stopwords.txt file.
     """
-    text = article.text
-    filename = open("/Users/z/Desktop/2022 Fall/OIM/text-mining/Assignments/stopwords.txt","r")
-    d = stop_words(filename)
+    text = clean_text(article.text)
+    # filename = open("/Users/z/Desktop/2022 Fall/OIM/text-mining/Assignments/stopwords.txt","r")
+    # d = stop_words(filename)
     freq = most_common(text)
+    print(freq)
+
+
+def senti():
+    """
+    Functions below are for Sentiment Intensity Analysis of the text, to see the percentage of positive/neutral/negative/compound
+    words used in the text.
+    """
+    sentence = article.text
+    score = SentimentIntensityAnalyzer().polarity_scores(sentence)
+    return score
+
+print(senti())
 
 if __name__ == "__main__":
     main()
-
-"""
-Functions below are for Sentiment Intensity Analysis of the text, to see the percentage of positive/neutral/negative/compound
-words used in the text.
-"""
-sentence = article.text
-score = SentimentIntensityAnalyzer().polarity_scores(sentence)
-print(score)
-
-
